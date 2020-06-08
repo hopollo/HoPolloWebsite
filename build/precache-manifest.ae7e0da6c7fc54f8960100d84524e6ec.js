a08c72e2bcf34f91ea5bdd4f859bc41af38f85ff
hopollo <hopollotv@gmail.com>
2020-06-08 21:04:57 +0300

    first react commit

---
+++ build/precache-manifest.ae7e0da6c7fc54f8960100d84524e6ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3964a1187750dcd78ed716d105b8d452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9a3cffffdf087fee1d",
+    "url": "/static/css/main.006a0316.chunk.css"
+  },
+  {
+    "revision": "7a9fe2bcaac411503faa",
+    "url": "/static/js/2.2889911a.chunk.js"
+  },
+  {
+    "revision": "a8708c1560a44a3a2f75343bf0ed33a1",
+    "url": "/static/js/2.2889911a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9a3cffffdf087fee1d",
+    "url": "/static/js/main.51518caf.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
